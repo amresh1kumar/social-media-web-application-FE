eadf4f556525c9cda2a9312bbf5580127a2fd475
amresh1kumar <amreshbkp0111@gmail.com>
2025-10-07 21:45:00 +0530

    update gitignore

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ export default function App() {
           <Route
             path="/chat"
             element={
+              // <PrivateRoute>
                 <Chat />
               // </PrivateRoute/>
             }
